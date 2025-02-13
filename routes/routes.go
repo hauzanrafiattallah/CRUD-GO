a5bf9570fbd2100e81a17d4a78aa8b95c2353c04
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-13 19:56:27 +0700

    CR-4 | feat: create user | CRUD USER

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,14 @@
 package routes
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/hauzanrafiattallah/CRUD-GO/controller"
 )
 
-func MapRoutes(server *http.ServeMux) {
+func MapRoutes(server *http.ServeMux, db *sql.DB) {
 	server.HandleFunc("/", controller.NewHelloWorldController())
-
+	server.HandleFunc("/user", controller.NewIndexUser())
+	server.HandleFunc("/user/create", controller.NewCreateUserController(db))
 }
